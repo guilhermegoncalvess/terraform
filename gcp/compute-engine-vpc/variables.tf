83cb5eaed78964d675332b2bedbee1d235409ec1
Guilherme Gonçalves dos Santos <guilherme.santos@climatempo.com.br>
2023-04-20 12:22:42 -0300

    feat: Create a vpc with subnet and compute engine

---
+++ gcp/compute-engine-vpc/variables.tf
@@ -0,0 +1,16 @@
+variable "project" {
+  default = ""
+}
+
+variable "credentials_file" {
+  default = ""
+}
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
+
